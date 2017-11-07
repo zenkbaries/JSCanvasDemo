adc8758a40470161ff7173dc77b1fd6a7707a4f4
John Towery <kbaries@gmail.com>
2017-11-07 07:29:38 -0700

    Added delay to raindrop

--- js/raindrop.js
+++ js/raindrop.js
@@ -1,35 +1,44 @@
 
 
 // Initialize
+
+// -- Declaration
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+var r = 100;
+var count = 0;
+
+// set canvas size
 c.width = window.innerWidth;
 c.height = window.innerHeight;
-
-r = 100;
 
 // Clear screen
 ctx.clearRect(0,0,c.width,c.height);
 
-
 // draw - where all the animation take place
 function draw() {
-  // draw circle
+  // fade the canvas
   ctx.globalAlpha=0.1;
   ctx.fillStyle="#FFFFFF";
   ctx.fillRect(0,0,c.width,c.height);
   ctx.globalAlpha=1.0;
   ctx.beginPath();
-  // ctx.arc(c.width/2,c.height/2,r,0,2*Math.PI);
-  ctx.arc(
-    Math.round(Math.random() * c.width),
-    Math.round(Math.random() * c.height),
-    Math.round(Math.random() * r),
-    0,
-    2*Math.PI
-  );
-  ctx.stroke();
-  ctx.closePath();
+
+  if (count == 10) {
+    // draw random raindrop
+    ctx.arc(
+      Math.round(Math.random() * c.width),
+      Math.round(Math.random() * c.height),
+      Math.round(Math.random() * r),
+      0,
+      2*Math.PI
+    );
+    ctx.stroke();
+    ctx.closePath();
+    count = 0;
+  }
+  count += 1;
+
 }
 
 // Initalize the animationFrame
